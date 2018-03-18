c6a75d4ac10d854000b0c61ed92ca9fc41bfa1f2
Binybaby <36341077+Binybaby@users.noreply.github.com>
2018-03-18 10:28:51 +0000

    Create loopsum.py

---
+++ loopsum.py
@@ -0,0 +1,11 @@
+# Biny Baby, 2018-03-18
+# Sum of all the numbers from 1 to 100
+
+sum = 0
+i = 0
+
+while i <= 100:
+  sum = sum + i
+  i = i + 1
+
+print("The sum of the numbers from 1 to 100 is:", sum)
